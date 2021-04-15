46ecbfc58b5e304e937a0265bd4663f826146dcd
puxa7 <umpik123@gmail.com>
2021-04-15 13:38:16 +0200

    pierwszy commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+wezly = np.array([[1, 0],
+                  [2, 1],
+                  [3, 0.5],
+                  [4, 0.75]])
+
+elementy = np.array([[1, 1, 3],
+                     [2, 4, 2],
+                     [3, 3, 4]])
+
+twb_L = 'D'
+twb_R = 'D'
+
+wwb_L = 0
+wwb_R = 1
