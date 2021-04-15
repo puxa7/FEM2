25b34b9c1a2eb56d51936bfa57241ee27d3087ff
puxa7 <umpik123@gmail.com>
2021-04-15 19:56:07 +0200

    drugi commit

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
+def generator(xstart,xstop,n):
 
-wezly = np.array([[1, 0],
-                  [2, 1],
-                  [3, 0.5],
-                  [4, 0.75]])
+    val = (xstop-xstart)/(n-1)
+    wezly = np.array([xstart])
 
-elementy = np.array([[1, 1, 3],
-                     [2, 4, 2],
-                     [3, 3, 4]])
+    for i in range(1,n,1):
+        wezly = np.block([wezly, i * val + xstart])
 
-twb_L = 'D'
-twb_R = 'D'
+    elementy = np.zeros((n-1,2))
 
-wwb_L = 0
-wwb_R = 1
+    for i in range(0, n-1, 1):
+        elementy[i][0] = i+1
+        elementy[i][1] = i+2
+
+    return wezly,elementy
+
+def plot_g(wezly,elementy, n):
+    tab=np.zeros((n))
+    l1 = plt.plot(wezly,tab)
+    for i in range(0,n-1,1):
+        plt.text(wezly[i], 0, "|")
+        plt.text(wezly[i+1]/2+wezly[i]/2, 0.003 ,str(i+1))
+        plt.text(wezly[i], -0.02, str(i+1))
+
+wezly, elementy = generator(0,1,4)
+plot_g(wezly, elementy, len(wezly))
+
